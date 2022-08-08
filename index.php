02c5a03f84c3ef6e4c88fda29c821e1d7eb69532
Priyanshu <coolpriyanshu236@gmail.com>
2022-08-08 22:32:42 +0530

    recent post

--- index.php
+++ index.php
@@ -54,7 +54,48 @@
            
     </div>
     </div>
-                </div>
+</div>
+<h1>Recent Posts</h1>
+<div class="album py-5 bg-light">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+               <?php
+                    $query = "SELECT id, postTitle, description, post_date, author, catinfo,imgUrl FROM blog_posts ORDER BY post_date DESC";
+                    $result = mysqli_query($con,$query);
+                    if(!$result || mysqli_num_rows($result)==0)
+                    {
+                        echo 'noting';
+                    }
+                    else
+                    {
+                        while($row= mysqli_fetch_assoc($result))
+                        {
+        
+                                $text=$row['description'];
+                                if(strlen($text)>100)
+                                {
+                                    $str2='...';
+                                    $text=substr($text,0,97);
+                                    $text=$text.$str2;
+                                }
+                                
+                                echo '<div class="col-lg-4 col-md-6 grid-item mb-4">';
+                                echo '<a href="viewpost.php?id='.$row['id'].'&title='.$row['postTitle'].'">';
+                                echo'<img class="img-fluid w-100 mb-3 img-thumbnail shadow-sm rounded-0" src='.$row['imgUrl'].' alt="">';
+                                echo '</a>';
+                                echo '<h2 class="h4">'.$row['postTitle'].'</h2>';
+                                echo '<p class="small text-muted font-italic">'.$text.'</p>';
+                                echo '</div>';
+                      
+                        }
+                    }
+               ?>
+           
+           
+    </div>
+    </div>
+</div>
 <footer class="p-4 bg-light d-flex align-items-center justify-content-center">
     <small class="mb-0 text-uppercase font-weight-bold mr-3">Resources: </small>
     <ul class="list-inline mb-0 d-inline-block">
